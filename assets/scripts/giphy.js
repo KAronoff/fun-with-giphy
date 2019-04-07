c7e6ad46bd714f84e364f0d48a82072d4ad07db3
Kit Aronoff <dreamnmusic@gmail.com>
2019-04-07 16:49:58 -0400

    Generated the images, saving both the still and animated url which was put into HTML data-still, data-animate and I added a data-state to indicate what the current state is. I added the API function to the search field. It clears the contents of the search after pressing submit. It also adds a button up top and generates the images. You can also generate images by clicking on the buttons on top because the API function was added to those as well. When submit or the buttons up top are clicked it clears the screen of gifs so it doesn't get too unweildy.

--- assets/scripts/giphy.js
+++ assets/scripts/giphy.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
 
 // array of buttons 
   var topicArr = ["Cat", "Lion", "Snow Leopard"];
-  //  other global variables: search input field
- 
+  //  other global variables: url paths
+ var stillURL;
+ var animatedURL;
 // function to display buttons 
   // function needs to take the input inside the search area and then add a button
   function displayBtn (topic){
@@ -25,20 +26,30 @@ $(document).ready(function(){
   // // a click event for the generated buttons 
 
   $(document).on("click", ".gifBtn", function(){
+    // clear the #giphyArea
+    $("#giphyArea").empty();
+
+    // create variable to save the data topic
+    var $dataTopic = $(this).attr("data-topic")
+    
     // when the user clicks on the button the page will add to the gif display area
+    searchForGiphy($dataTopic);
   })
   // // click event for the search (submit) button
   $("#searchBtn").on("click", function(){
     event.preventDefault();
     // take the information in the input field and save it as a variable
+    $("#giphyArea").empty();
     var searchInputVar = $("#searchTxt").val()
     console.log(searchInputVar);
     // display the new button
     displayBtn(searchInputVar);
-    // clear the search field
-    $("#searchTxt").val("");
+    
     // have the variable be able to run through the ajax search function
     searchForGiphy(searchInputVar);
+
+    // clear the search field
+    $("#searchTxt").val("");
   })
 
   function searchForGiphy (searchField){
@@ -46,11 +57,34 @@ $(document).ready(function(){
     var queryURL = "https://api.giphy.com/v1/gifs/search?"
     var parameters = {"api_key":"pWCQRgiCN7dmL6YFmnlCyJz6px01doDw"};
     parameters.q = searchTermVar;
+    parameters.limit = 10;
+    parameters.rating = "g";
     $.ajax ({
       url: queryURL+$.param(parameters),
       method: "GET"
     }).then(function(giphyResult){
       console.log(giphyResult);
+      // things needed: still image, animated image, rating
+      // animated downsized image url path: giphyResult.data[i].images.downsized.url
+      // still downsized image url path: giphyResult.data[i].images.downsized_still.url
+      for (i=0; i < parameters.limit ; i++){
+        animatedURL = giphyResult.data[i].images.fixed_width_small.url;
+        stillURL = giphyResult.data[i].images.fixed_width_small_still.url;
+      
+      var $resultImg = $("<img>")
+      .addClass(".imgGifResult")
+      .attr("src", stillURL)
+      .attr("data-still", stillURL)
+      .attr("data-animate", animatedURL)
+      .attr("data-state", "still");
+
+      $("#giphyArea").append($resultImg);
+      }
+      
     })
+  }
+  function stillToAnimated (url){
+    var dataForStillToAnimation = $(this).attr("data-topic")
+    
   }
 });
